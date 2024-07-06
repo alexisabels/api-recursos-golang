eeb2363103b00243e06664c515281e9e004e0178
alexisabels <isabelsanchezalex@gmail.com>
2024-07-07 01:32:49 +0200

    vercel 6

--- handlers/categorias.go
+++ handlers/categorias.go
@@ -4,38 +4,53 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetLenguajesProgramacion(c *gin.Context) {
-    data, err := os.ReadFile("./data/categorias/lenguajes-de-programacion.json")
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	dir, err := os.Getwd()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo obtener el directorio de trabajo"})
+		return
+	}
+	filePath := filepath.Join(dir, "data", "categorias", "lenguajes-de-programacion.json")
 
-    var categoriaData interface{}
-    if err := json.Unmarshal(data, &categoriaData); err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-    c.JSON(http.StatusOK, categoriaData)
+	var categoriaData interface{}
+	if err := json.Unmarshal(data, &categoriaData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, categoriaData)
 }
+
 func GetCategorias(c *gin.Context) {
-    data, err := os.ReadFile("./data/categorias/categorias.json") 
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	dir, err := os.Getwd()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo obtener el directorio de trabajo"})
+		return
+	}
+	filePath := filepath.Join(dir, "data", "categorias", "categorias.json")
 
-    var categoriasData interface{}
-    if err := json.Unmarshal(data, &categoriasData); err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-    c.JSON(http.StatusOK, categoriasData)
+	var categoriasData interface{}
+	if err := json.Unmarshal(data, &categoriasData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, categoriasData)
 }
-
